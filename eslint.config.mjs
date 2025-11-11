8d61bfa6f51091c51080f79111608f7eda6eb179
Roger Santos <roger.santos36@gmail.com>
2025-11-11 14:17:01 -0300

    chore: configurar prettier e eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,39 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginVue from "eslint-plugin-vue";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  plugins: { eslintPluginVue, eslintPluginImport, eslintPluginPrettier },
+  rules: {
+    // Geral
+    "no-console": "warn",
+    "no-debugger": "error",
+    "prefer-const": "warn",
+
+    // Importações
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        // alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+
+    // Vue
+    "vue/multi-word-component-names": "off",
+    "vue/no-unused-vars": "warn",
+    "vue/no-mutating-props": "error",
+    "vue/html-self-closing": "off",
+
+    // Prettier (opcional se usa Prettier)
+    "eslintPluginPrettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        semi: true,
+      },
+    ],
+  },
+});
