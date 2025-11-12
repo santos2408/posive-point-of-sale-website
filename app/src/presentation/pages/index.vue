80ee4c6f99dc22277695151a3f8ca5585b5a9b34
Roger Santos <roger.santos36@gmail.com>
2025-11-12 14:22:43 -0300

    feat: add HeaderContainer on index page

---
+++ app/src/presentation/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HeaderContainer from "@/presentation/modules/header/containers/HeaderContainer.vue";
+</script>
+
+<template>
+  <div class="w-full">
+    <HeaderContainer />
+    <!-- <main>main content</main> -->
+  </div>
+</template>
