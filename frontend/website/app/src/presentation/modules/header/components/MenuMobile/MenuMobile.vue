99f894bfdf277acf9e2840571a8b87b14d871553
Roger Santos <roger.santos36@gmail.com>
2026-02-15 12:15:06 -0300

    refactor: ajustar cor de fundo e hover do MenuMobile

--- frontend/website/app/src/presentation/modules/header/components/MenuMobile/MenuMobile.vue
+++ frontend/website/app/src/presentation/modules/header/components/MenuMobile/MenuMobile.vue
@@ -24,7 +24,7 @@ onClickOutside(menu, () => {
 
 <template>
   <div class="menu-overlay fixed top-0 left-0 z-50 w-full">
-    <nav ref="menu" class="bg-brand-neutral-500 z-50 h-screen w-4/5 px-4 py-8">
+    <nav ref="menu" class="bg-brand-neutral-900 z-50 h-screen w-4/5 px-4 py-8">
       <div class="mb-10 flex items-center justify-between">
         <a href="/">
           <ClientOnly>
@@ -48,7 +48,9 @@ onClickOutside(menu, () => {
             idx === props.items.length - 1 ? 'border-b' : undefined,
           ]"
         >
-          <NuxtLink :to="menuItem.link" class="block cursor-pointer py-3">{{ menuItem.label }}</NuxtLink>
+          <NuxtLink :to="menuItem.link" class="hover:text-brand-primary-500 block cursor-pointer py-3">
+            {{ menuItem.label }}
+          </NuxtLink>
         </li>
       </ul>
 
