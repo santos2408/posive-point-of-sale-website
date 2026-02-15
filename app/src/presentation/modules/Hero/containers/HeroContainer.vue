0fae11934eafa11413a33f9501fc882c197798f8
Roger Santos <roger.santos36@gmail.com>
2026-02-15 17:21:23 -0300

    feat: criar componente inicial da HeroContainer

---
+++ app/src/presentation/modules/Hero/containers/HeroContainer.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useElementStore } from "@/presentation/stores/element";
+
+// states
+const hero = useTemplateRef("hero");
+
+// composables
+const { width } = useWindowSize();
+const elementStore = useElementStore();
+
+// methods
+onMounted(() => {
+  const header = elementStore.getElement("header");
+
+  if (header && hero.value) {
+    hero.value.style.paddingTop = `${header.clientHeight}px`;
+  }
+});
+
+watch(width, () => {
+  if (import.meta.client) {
+    const header = elementStore.getElement("header");
+
+    if (header && hero.value) {
+      const header = elementStore.getElement("header");
+      hero.value.style.paddingTop = `${header?.clientHeight}px`;
+    }
+  }
+});
+</script>
+
+<template>
+  <section ref="hero">
+    <h1>Empower Your Business Growth with Our Point of Sale</h1>
+  </section>
+</template>
--- app/src/presentation/pages/index.vue
+++ app/src/presentation/pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import HeaderContainer from "@/presentation/modules/header/containers/HeaderContainer.vue";
+import HeroContainer from "@/presentation/modules/Hero/containers/HeroContainer.vue";
 </script>
 
 <template>
   <div class="w-full">
     <HeaderContainer />
-    <!-- <main>main content</main> -->
+    <main>
+      <HeroContainer />
+    </main>
   </div>
 </template>
