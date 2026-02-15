84b3b319ab8609b9a5bb08cbf08ab6392bf68c66
Roger Santos <roger.santos36@gmail.com>
2026-02-15 17:33:07 -0300

    chore: ajustar eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,17 @@
-import { defineConfig } from "eslint/config";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintPluginPrettier from "eslint-plugin-prettier";
-import websiteConfig from "./frontend/website/eslint.config.js";
 
-export default withNuxt([
-  // ================================
-  //         GLOBAL SETTINGS
-  // ================================
-  {
-    plugins: { prettier: eslintPluginPrettier },
-    basePath: "./frontend",
-    ignores: ["**/node_modules/**", "**/.nuxt/**", "**/dist/**"],
-    files: ["./frontend/**/**/*.{js,ts,vue}"],
-    rules: {
-      "no-console": "warn",
-      "no-debugger": "error",
-      "prefer-const": "warn",
-      "vue/multi-word-component-names": "off",
+export default withNuxt({
+  files: ["**/*.{js,ts,vue}"],
+  plugins: { prettier: eslintPluginPrettier },
+  ignores: ["**/node_modules/**", "**/.nuxt/**", "**/dist/**"],
+  rules: {
+    "no-console": "warn",
+    "no-debugger": "error",
+    "prefer-const": "warn",
+    "vue/multi-word-component-names": "off",
     "vue/no-unused-vars": "warn",
     "vue/no-mutating-props": "error",
     "vue/html-self-closing": "off",
-    "no-console": "warn",
-    },
   },
-]);
+});
