cb7eea556867fe09ba4af559964e0dace38c1217
Roger Santos <roger.santos36@gmail.com>
2026-02-19 12:10:15 -0300

    feat: adicionar imagens background hero e ipad hero e ajustar responsividade delas

--- app/src/presentation/modules/Hero/containers/HeroContainer.vue
+++ app/src/presentation/modules/Hero/containers/HeroContainer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useElementStore } from "@/presentation/stores/element";
+import ActionButton from "~/presentation/components/ActionButton/components/ActionButton.vue";
 
 // states
 const hero = useTemplateRef("hero");
@@ -30,7 +31,47 @@ watch(width, () => {
 </script>
 
 <template>
-  <section ref="hero">
-    <h1>Empower Your Business Growth with Our Point of Sale</h1>
+  <section ref="hero" class="relative">
+    <div class="container py-12 text-center">
+      <p class="text-brand-primary-500 mb-2.5 text-sm font-bold uppercase">#1 best All-in-one POS platform</p>
+
+      <h1 class="text-brand-neutral-900 mb-4 text-3xl font-extrabold">
+        Empower Your <br class="max-sm:hidden" />
+        Business Growth with <br class="max-sm:hidden" />
+        Our Point of Sale
+      </h1>
+
+      <p class="text-brand-neutral-400 mb-6 font-medium">
+        From managing inventory to tracking sales performance, our platform provides you with the tools you need to
+        succeed in today's competitive market
+      </p>
+
+      <div class="space-y-4">
+        <ActionButton label="Start free trial" class="w-full" />
+        <ActionButton
+          label="Watch show reel"
+          :style="'action-button--outlined'"
+          icon="cuida:play-outline"
+          class="text-brand-neutral-900 w-full font-bold"
+        />
+      </div>
+    </div>
+
+    <div class="relative">
+      <picture>
+        <!-- <source media="(max-width: 600px)" srcset="produto-mobile-recortado.jpg" /> -->
+        <!-- <source media="(max-width: 1024px)" srcset="produto-tablet-medio.jpg" /> -->
+        <img src="/images/hero/hero-background.png" alt="Pessoa usando produto" aria-hidden="true" />
+      </picture>
+
+      <div class="absolute -top-10 left-0 flex h-full w-full items-center justify-center">
+        <img
+          src="/images/hero/hero-ipad-device.png"
+          srcset="/images/hero/hero-ipad-device-350w.png 350w"
+          sizes="(max-width:480px) 350px"
+          alt="Painel de vendas da aplicação Posive"
+        />
+      </div>
+    </div>
   </section>
 </template>
