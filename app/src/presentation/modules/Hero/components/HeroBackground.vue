655e430425c44477b5ab8d3b6302b190baee1917
Roger Santos <roger.santos36@gmail.com>
2026-02-20 10:21:58 -0300

    refactor: adicionar imagens otimizadas

--- app/src/presentation/modules/Hero/components/HeroBackground.vue
+++ app/src/presentation/modules/Hero/components/HeroBackground.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="relative">
     <picture>
-      <source media="(min-width: 1260px)" srcset="/images/hero/hero-background-1260w.png" />
-      <source media="(min-width: 1180px)" srcset="/images/hero/hero-background-1180w.png" />
-      <source media="(min-width: 1024px)" srcset="/images/hero/hero-background-tablet.png" />
+      <source media="(min-width: 1260px)" srcset="/images/hero/optimized/hero-background-1260w.avif" />
+      <source media="(min-width: 1180px)" srcset="/images/hero/optimized/hero-background-1180w.avif" />
+      <source media="(min-width: 1024px)" srcset="/images/hero/optimized/hero-background-tablet.avif" />
 
       <img
-        src="/images/hero/hero-background-mobile.png"
+        src="/images/hero/optimized/hero-background-mobile.avif"
         alt=""
         aria-hidden="true"
         class="w-full object-cover object-center md:max-h-145 xl:max-h-200"
@@ -15,9 +15,12 @@
 
     <div class="absolute -top-6 left-0 flex h-full w-full items-center justify-center md:top-0">
       <img
-        srcset="/images/hero/hero-ipad-device-350w.png 350w, /images/hero/hero-ipad-device.png 864w"
+        srcset="
+          /images/hero/optimized/hero-ipad-device-350w.avif 350w,
+          /images/hero/optimized/hero-ipad-device.avif      864w
+        "
         sizes="(max-width:480px) 350px, (max-width:768px) 864px"
-        src="/images/hero/hero-ipad-device.png"
+        src="/images/hero/optimized/hero-ipad-device.avif"
         alt="Painel de vendas da aplicação Posive"
         class="w-full max-w-164 px-6 lg:px-0 xl:max-w-216"
       />
